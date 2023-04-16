1bb372453616e6a9386cee3f160175754fad70cf
Yutaro <yutaro.fksm.sep.09@gmail.com>
2023-04-16 19:28:27 +0900

    topic-7が終わりました

---
+++ AOJ/07/07_B.py
@@ -0,0 +1,12 @@
+while True:
+    n, x=map(int,input().split())
+    if n==x==0:
+        break
+    t = 0
+    for a in range(1,n+1):
+        for b in range(1,a):
+            for c in range(1,b):
+                if a + b + c == x:
+                    t+=1
+    print(t)
+    t=0

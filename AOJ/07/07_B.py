596ed696f15c356d3d82dbaa57783dcc68999fc8
Yutaro <yutaro.fksm.sep.09@gmail.com>
2023-04-19 22:41:52 +0900

    修正後

--- AOJ/07/07_B.py
+++ AOJ/07/07_B.py
@@ -1,6 +1,6 @@
 while True:
-    n, x=map(int,input().split())
-    if n==x==0:
+    n, x = map(int,input().split())
+    if n == x == 0:
         break
     t = 0
     for a in range(1,n+1):
@@ -9,4 +9,3 @@ while True:
                 if a + b + c == x:
                     t+=1
     print(t)
-    t=0
